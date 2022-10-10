346d13bdb91277653038ad5b74491947197a555f
szamotine <77281802+szamotine@users.noreply.github.com>
2022-10-10 10:53:23 -0700

    TODO grey brick initialization

--- terrain.cpp
+++ terrain.cpp
@@ -23,13 +23,45 @@ void terrain::initialize_terrain() {
 void terrain::initialize_red_bricks() {}
 
 void terrain::initialize_grey_bricks() {
-	int n = 18;
+	//TODO : refactor commented code to produce checkerboard pattern with grey bricks
 
+	int n = 18;
+	//This can be removed, its a test to configure gb.list
 	for (double i = 1; i <= n; i++) {
 
 		grey_brick temp = grey_brick(((i * 42) - 21), ((i * 42) - 21));
 		grey_brick_list.push_back(grey_brick(temp));
 	}
+	/*
+	// draw the countour of the map 
+	// current iteration repeat all 4 corners
+	// would be better to replace with just the right amount of entities
+	for (int i = 1; i < n; i++) {
+		draw_sprite(grey_bricks_sprite_id, x_buffer[i], 21, bricks_angle, bricks_scale);
+		draw_sprite(grey_bricks_sprite_id, x_buffer[i], 693, bricks_angle, bricks_scale);
+		draw_sprite(grey_bricks_sprite_id, 21, y_buffer[i], bricks_angle, bricks_scale);
+		draw_sprite(grey_bricks_sprite_id, 693, y_buffer[i], bricks_angle, bricks_scale);
+	}
+
+	// the value 49 is found from the loop below
+	n_grey_bricks = 49;
+	x_grey_bricks = new double[n_grey_bricks];
+	y_grey_bricks = new double[n_grey_bricks];
+
+	int count = 0;
+
+	// create the check pattern 
+	// we jump one element of the array so that we have a checkboard pattern
+	// to vary the height, we use the number of pixels as a reference and a small arithmetic calculation
+	for (int i = 3; i < n - 2; i = i + 2) {
+		for (int j = 0; j < 7; j++) {
+			draw_sprite(grey_bricks_sprite_id, x_buffer[i], double((j * 84.0) + 105.0), bricks_angle, bricks_scale);
+			x_grey_bricks[count] = x_buffer[i];
+			y_grey_bricks[count] = double((j * 84.0) + 105.0);
+			count++;
+		}
+	}
+	*/
 
 	cout << "\nGrey Bricks Initialized: " << grey_brick_list.size();
 }
