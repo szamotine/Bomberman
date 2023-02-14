21946b3939619d17c711cc84767d72b345bd5bf9
szamotine <77281802+szamotine@users.noreply.github.com>
2023-02-14 10:21:26 -0800

    Grey brick sonarlint

--- grey_brick.h
+++ grey_brick.h
@@ -5,16 +5,19 @@ private:
 
 	double x_coordinate;
 	double y_coordinate;
-
+	int i_index;
+	int j_index;
 
 public:
 
 	grey_brick(double x, double y);
-	grey_brick(grey_brick& source);
 	~grey_brick();
 
-	double get_x_coordinate();
-	double get_y_coordinate();
+	double get_x_coordinate() const;
+	double get_y_coordinate() const;
+
+	int get_i_index() const;
+	int get_j_index() const;
 
 };
 
