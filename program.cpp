47457f2e4c742ca2ab067110a5ad350d3bdaa6a1
szamotine <77281802+szamotine@users.noreply.github.com>
2023-02-14 10:40:33 -0800

    sonarlint program

--- program.cpp
+++ program.cpp
@@ -8,8 +8,6 @@
 //#include "ran.h"
 #include "world.h"
 
-using namespace std;
-
 static int init = 0; // initialization flag
 static double t; // clock time from start of program
 static double t0; // initial clock time
@@ -17,7 +15,8 @@ static double t0; // initial clock time
 // the previous function call.
 // we want tp static so it's remembered between function calls
 static double tp;
-double T, fps;
+double T;
+double fps;
 
 
 void FPS();
@@ -26,36 +25,33 @@ void initialize();
 
 world W1 = world();
 
-int main()
-{
-		
+
+int main() {
+
 	// graphics drawing / animation loop
-	while(1) {
-		
-		if (!init) 
+	while (true)
+	{
+
+		if (!init)
 		{
 			initialize();
-			cout << "\nStarting game";
+			std::cout << "\nStarting game";
 		}
 
 		clear(); // clear the previous drawing
-
-		
 
 		W1.run();
 		grid_overlay();
 		FPS();
 		update(); // update the drawing
 
-
 	}
-	
-	
+
+
 	return 0;
 }
 
-void FPS() 
-{
+void FPS() {
 	// read clock time (resolution is 0.1 microseconds)
 	t = high_resolution_time() - t0; // time since the program started (s)
 
@@ -83,8 +79,7 @@ void FPS()
 
 }
 
-void grid_overlay() 
-{
+void grid_overlay() {
 	int x = 42;
 	int y = 21;
 
@@ -112,8 +107,6 @@ void initialize() {
 
 	tp = t; // initialize the previous clock time
 
-	
-
 	// initialize and setup the 2D graphics library
 	initialize_graphics();
 
@@ -122,6 +115,6 @@ void initialize() {
 	W1.initialize_world();
 	init = 1;
 
-	cout << "\nInitialization section complete";
+	std::cout << "\nInitialization section complete";
 
 }
