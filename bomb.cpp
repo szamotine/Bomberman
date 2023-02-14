e315319b47bd814183455f54afc3e66050d467db
szamotine <77281802+szamotine@users.noreply.github.com>
2023-02-14 07:20:25 -0800

    Clean Sonarlint bomb

--- bomb.cpp
+++ bomb.cpp
@@ -4,32 +4,17 @@
 #include "bomb.h"
 #include "calculator.h"
 
-bomb::~bomb() {
-	
-}
-/*
-bomb::bomb(double x, double y, double t) {
-	x_coordinate = x;
-	y_coordinate = y;
-	time = t;
-	bomb_removal_flag = false;
-	set_bomb_indices(x, y);
-	
-	//std::cout << "\nBomb Constructed at i: " << i_index << ", j: " << j_index;
-	
-}
-*/
+#pragma region Constructors
 
-bomb::bomb(int i, int j, double t)
-{
-	i_index = i;
-	j_index = j;
+bomb::~bomb() = default;
+bomb::bomb(int i, int j, double t) :time(t), i_index(i), j_index(j) {
 	x_coordinate = calculator::calculate_coordinate(i);
 	y_coordinate = calculator::calculate_coordinate(j);
-	time = t;
-	bomb_removal_flag = false;
-
 }
+
+#pragma endregion
+
+#pragma region Setters
 
 void bomb::set_x_coordinate(double value) {
 	x_coordinate = value;
@@ -40,37 +25,31 @@ void bomb::set_y_coordinate(double value) {
 void bomb::set_time(double value) {
 	time = value;
 }
-void bomb::set_bomb_removal_flag()
-{
+void bomb::set_bomb_removal_flag() {
 	bomb_removal_flag = true;
 }
-void bomb::set_bomb_indices(double x, double y)
-{
-	this->i_index = calculator::calculate_index(x);
-	this->j_index = calculator::calculate_index(y);
-}
-double bomb::get_x_coordinate() {
+
+#pragma endregion
+
+#pragma region Getters
+
+double bomb::get_x_coordinate() const {
 	return x_coordinate;
 }
-double bomb::get_y_coordinate() {
+double bomb::get_y_coordinate() const {
 	return y_coordinate;
 }
-
-double bomb::get_time() {
-	return time;
-}
-
-bool bomb::get_bomb_removal_flag()
-{
+bool bomb::get_bomb_removal_flag() const {
 	return bomb_removal_flag;
 }
-
-int bomb::get_bomb_i_index()
-{
+double bomb::get_time() const {
+	return time;
+}
+int bomb::get_bomb_i_index() const {
 	return i_index;
 }
-
-int bomb::get_bomb_j_index()
-{
+int bomb::get_bomb_j_index() const {
 	return j_index;
 }
+
+#pragma endregion
--- bomb.h
+++ bomb.h
@@ -1,30 +1,36 @@
 #pragma once
 
-class bomb 
+class bomb
 {
 private:
 	double x_coordinate;
 	double y_coordinate;
 	double time;
-	bool bomb_removal_flag;
-	int i_index, j_index;
-	
+	bool bomb_removal_flag = false;
+	int i_index;
+	int j_index;
+
 public:
 
+#pragma region Constructors
 	~bomb();
-//	bomb(double x, double y, double t);
 	bomb(int i_index, int j_index, double t);
+#pragma endregion
 
+#pragma region Setters
 	void set_x_coordinate(double value);
 	void set_y_coordinate(double value);
 	void set_time(double value);
 	void set_bomb_removal_flag();
-	void set_bomb_indices(double x, double y);
 
-	double get_x_coordinate();
-	double get_y_coordinate();
-	double get_time();
-	bool get_bomb_removal_flag();
-	int get_bomb_i_index();
-	int get_bomb_j_index();
+#pragma endregion
+
+#pragma region Getters
+	double get_x_coordinate() const;
+	double get_y_coordinate() const;
+	double get_time() const;
+	bool get_bomb_removal_flag() const;
+	int get_bomb_i_index() const;
+	int get_bomb_j_index() const;
+#pragma endregion
 };
